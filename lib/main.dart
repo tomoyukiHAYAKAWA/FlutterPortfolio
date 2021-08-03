b4fcf0f081e04248c03c7f9e035932c77dbe1c4e
Tomoyuki Hayakawa <tomoyukihayakawa@TomoyukinoMacBook-Pro.local>
2021-08-04 01:50:15 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'my space',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'my space'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Center(
+                child: Text(
+                  'my space\nindex',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text(
+                'about me',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.build),
+              title: Text(
+                'skils',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.place),
+              title: Text(
+                'timeline',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.link),
+              title: Text(
+                'links',
+                style: TextStyle(fontSize: 16),
+              ),
+            )
+          ],
+        ),
+      ),
+      // about me
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(16),
+              child: Text(
+                'about me',
+                style: TextStyle(fontSize: 28),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(8),
+              width: 200.0,
+              height: 200.0,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      fit: BoxFit.fill, image: AssetImage("self.png"))),
+            ),
+            Container(
+              margin: EdgeInsets.all(8),
+              child: Text(
+                'Tomoyuki Hayakawa',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(8),
+              child: Text(
+                '1995年生まれ / 名古屋出身 / 東京都在住 / iOS App Developer',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
