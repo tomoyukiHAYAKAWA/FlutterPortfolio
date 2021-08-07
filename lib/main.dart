d7ca1f5c338c214e5bd66ed28ce6cbb093646b2a
Tomoyuki Hayakawa <tomoyukihayakawa@TomoyukinoMacBook-Pro.local>
2021-08-08 03:00:07 +0900

    プロフィールをカードで表示させ、スキルをグリッドで実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,11 +9,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const locale = Locale("ja", "JP");
     return MaterialApp(
       title: 'my space',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      locale: locale,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        locale,
+      ],
       home: MyHomePage(title: 'my space'),
     );
   }
@@ -32,103 +44,146 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Center(
-                child: Text(
-                  'my space\nindex',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text(
-                'about me',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.build),
-              title: Text(
-                'skils',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.place),
-              title: Text(
-                'timeline',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.link),
-              title: Text(
-                'links',
-                style: TextStyle(fontSize: 16),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  flipCard(),
+                  skilArea(),
+                ],
               ),
             )
           ],
-        ),
-      ),
-      // about me
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[profileArea()],
         ),
       ),
     );
   }
 }
 
-Widget profileArea() {
-  return Container(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.all(16),
-          child: Text(
-            'about me',
-            style: TextStyle(fontSize: 28),
+Widget flipCard() {
+  return FlipCard(
+    front: profileAreaFlont(),
+    back: profileAreaBack(),
+    direction: FlipDirection.HORIZONTAL,
+    flipOnTouch: true,
+  );
+}
+
+Widget profileAreaFlont() {
+  return SizedBox(
+    height: 380.0,
+    width: 500.0,
+    child: Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(25.0),
+        ),
+      ),
+      margin: EdgeInsets.all(24.0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin:
+                EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0, bottom: 0.0),
+            padding: EdgeInsets.all(12.0),
+            width: 200.0,
+            height: 200.0,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    fit: BoxFit.fill, image: AssetImage("self.png"))),
           ),
+          Container(
+            margin:
+                EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0, bottom: 0.0),
+            padding: EdgeInsets.all(12.0),
+            child: Text(
+              'Tomoyuki Hayakawa',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            margin:
+                EdgeInsets.only(left: 8.0, right: 8.0, top: 0.0, bottom: 0.0),
+            padding: EdgeInsets.all(12.0),
+            child: Text(
+              '1995年生まれ / 名古屋出身 / 東京都在住 / iOS App Developer',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget profileAreaBack() {
+  return SizedBox(
+    height: 380.0,
+    width: 500.0,
+    child: Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(25.0),
         ),
-        Container(
-          margin: EdgeInsets.all(8),
-          width: 200.0,
-          height: 200.0,
-          decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                  fit: BoxFit.fill, image: AssetImage("self.png"))),
+      ),
+      margin: EdgeInsets.all(24.0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin:
+                EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0, bottom: 8.0),
+            padding: EdgeInsets.all(12.0),
+            child: Text(
+              '名古屋市出身のエンジニアです。学生時代はiOSアプリの個人開発をしていました。現在は都内の会社でiOSエンジニアをしています。サウナと自転車が好きです。好きな犬種はシベリアンハスキー、アラスカンマラミュート、チャウチャウ、柴犬。カードフリップを実装してみたくて自己紹介を書いてみました。',
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget skilArea() {
+  var list = [
+    _generator("AAA"),
+    _generator("BBB"),
+    _generator("CCC"),
+    _generator("DDD"),
+  ];
+  return GridView.count(
+      crossAxisCount: 3,
+      shrinkWrap: true,
+      // スクロールさせない
+      primary: false,
+      // physics: NeverScrollableScrollPhysics(),
+      children: list);
+}
+
+Widget _generator(String skilText) {
+  return SizedBox(
+    width: 190.0,
+    height: 190.0,
+    child: Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(25.0),
         ),
-        Container(
-          margin: EdgeInsets.all(8),
-          child: Text(
-            'Tomoyuki Hayakawa',
+      ),
+      margin: EdgeInsets.all(24.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            skilText,
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.all(8),
-          child: Text(
-            '1995年生まれ / 名古屋出身 / 東京都在住 / iOS App Developer',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ),
-      ],
+          )
+        ],
+      ),
     ),
   );
 }
